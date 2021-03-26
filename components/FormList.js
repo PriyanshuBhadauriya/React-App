a6b16005037fb7efe943579729bdd8bff365b27a
PriyanshuBhadauriya <62997069+PriyanshuBhadauriya@users.noreply.github.com>
2021-03-26 12:06:37 +0530

    Update FormList.js

--- components/FormList.js
+++ components/FormList.js
@@ -1,1 +1,41 @@
 
+import React, { Component } from 'react'
+import axios from 'axios'
+class PostList extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+      posts: [],
+      errorMsg: ''
+		}
+	}
+
+	componentDidMount() {
+		axios
+			.get('https://jsonplaceholder.typicode.com/posts')
+			.then(response => {
+				console.log(response)
+				this.setState({ posts: response.data })
+			})
+			.catch(error => {
+        console.log(error)
+        this.setState({errorMsg: ''})
+			})
+	}
+
+	render() {
+		const { posts, errorMsg } = this.state
+		return (
+			<div>
+				
+				{posts.length
+					? posts.map(post => <div key={post.rollno}>{post.result}</div>)
+          : null}
+        {errorMsg ? <div>{errorMsg}</div> : null}
+			</div>
+		)
+	}
+}
+
+export default PostList
