574d550493e6acbed732f3951e661c83ff7278be
PriyanshuBhadauriya <62997069+PriyanshuBhadauriya@users.noreply.github.com>
2021-03-26 12:15:27 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import './App.css'
 import PostList from './components/PostList'
 import PostForm from './components/PostForm'
+//github link"https://github.com/PriyanshuBhadauriya/React-App"
 
 
 class App extends Component {
